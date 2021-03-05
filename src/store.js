ca9e10fb6ecbb3b46c07d896df2f67c14398a519
Kosanyak19 <kosvik264@gmail.com>
2021-03-05 13:29:12 +0200

    commit-two

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import tasksReducer from './tasks/tasks.reducer';
 
-const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-  },
+
+const reducer = combineReducers({
+  tasks: tasksReducer
 });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,composeEnhancers(applyMiddleware(thunk))
+  
+);
 
 export default store;
