03fce5b6382caea762f05ab2ad2dd3a4cdbfaf5b
Kosanyak19 <kosvik264@gmail.com>
2021-03-05 14:34:56 +0200

    commit-readme

---
+++ README.md
@@ -0,0 +1,44 @@
+<div align="center">
+  <img width="100%" src="https://ibb.co/DKvB6c6">
+  <h1 style="margin-bottom:20px; margin-top:20px;">TODO-list with React-Redux</h1>
+</div>
+
+
+## Specification:
+
+Simple TODO-list with async react-redux
+
+## Build Setup:
+
+``` bash
+# Download repository:
+git clone https://github.com/Kosanyak19/todo-list.git
+
+# Go to the app:
+cd todo-list
+
+# Install dependencies:
+npm install
+
+# Server with hot reload at http://localhost:8080/
+npm start
+
+# Output will be at dist/ folder
+npm run build
+```
+
+## Tech Staks:
+
+*  HTML
+*  CSS3
+*  Flexbox
+*  Sass (SCSS)
+*  BEM methodology
+*  Webpack
+*  React 
+*  Redux
+
+
+<div><h1></h1></div>
+
+Copyright (c) 2021-present, [Kosanyak Viktor] (https://github.com/Kosanyak19)
--- src/store.js
+++ src/store.js
@@ -1,17 +1,28 @@
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+// import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import tasksReducer from './tasks/tasks.reducer';
+
+
+// const reducer = combineReducers({
+//   tasks: tasksReducer
+// });
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   reducer,composeEnhancers(applyMiddleware(thunk))
+  
+// );
+
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
 import tasksReducer from './tasks/tasks.reducer';
 
-
-const reducer = combineReducers({
-  tasks: tasksReducer
+const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+  },
 });
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  reducer,composeEnhancers(applyMiddleware(thunk))
-  
-);
 
 export default store;
